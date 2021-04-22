9c23417ad09ff4c254c800522b66e7175228cd48
derek-5 <wjn123562077@gmail.com>
2021-04-22 13:21:38 -0400

    try queue

---
+++ app/Jobs/TranslateSlug.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+use App\Models\Topic;
+
+class TranslateSlug implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $topic;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Topic $topic)
+    {
+        $this->topic = $topic;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $slug = 'testing slug ' . date("F j, Y, g:i a");
+        \DB::table('topics')->where('id',$this->topic->id)->update(['slug'=>$slug]);
+    }
+}
